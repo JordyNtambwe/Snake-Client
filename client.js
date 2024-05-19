fa6592430ac74d53e839c16b28e7594689ca5526
JordyNtambwe <jordyntambwe@gmail.com>
2024-05-18 21:41:16 -0600

    Move the connect function from play.js into client.js!

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+const net = require("net");
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -15,4 +17,4 @@ const connect = function () {
   return conn;
 };
 
-module.exports = connect
+module.exports = connect;
