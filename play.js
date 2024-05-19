2ffa71b38a47d7d2b4ff0d6b033f313fbc7c38d8
JordyNtambwe <jordyntambwe@gmail.com>
2024-05-18 21:11:59 -0600

    Connect To The Snake Server and Listen For Incoming Data!

--- play.js
+++ play.js
@@ -1,21 +1,16 @@
 const net = require("net");
-const Ip = "172.19.167.29"
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: Ip,
+    host: "localhost",
     port: 50541
   });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
-
-  conn.on("connect", () => {
-    conn.write("you ded cuz you idled")
-  });
-
+  
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
